ee1716b4f82f55255fdc6508ff3188633a74d238
tuckkiez <kanawut.netpol@kingpower.com>
2021-10-28 22:21:02 +0700

    change bot

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import re
 
 import random
 
-bot_token = '1950492866:AAFTEoytePceGMXmMdQ4tYdYZ3gbmgDs6Vo'
+bot_token = '2068478237:AAHQ3iKHt3f0TdiLFvn3pv8JjOTZMqn2PS8'
 #https://api.telegram.org/bot1950492866:AAHoMApimzGSG3XOOCaTFfRhj7EUTzcSqpo/getMe
 #https://api.telegram.org/bot1950492866:AAHoMApimzGSG3XOOCaTFfRhj7EUTzcSqpo/setWebhook
 
